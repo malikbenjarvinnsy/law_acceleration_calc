77cc6cd7821de697f0c81e5f2118fb61590772ce
Malik Benj Arvin N. Sy <your-email@example.com>
2025-07-13 08:51:39 +0800

    Added Direction Input

--- law_acceleration_calc.py
+++ law_acceleration_calc.py
@@ -1,23 +1,39 @@
 def get_force(m, a):
+    """Force = mass * acceleration"""
     return m * a
 
 def get_mass(f, a):
+    """Mass = force / acceleration"""
     return f / a
 
 def get_acceleration(f, m):
+    """Acceleration = force / mass"""
     return f / m
 
-objective = input("Quantity to be calculated (Force, Mass, Acceleration): ")
+print("Type the assigned number of your requested quantity.")
+print("Inputs are case-sensitive. Please enter in lowercase.")
+objective = input("Force (1), Mass (2), Acceleration (3): ")
 
-if objective == "force":
-    mass = float(input("Enter object's mass (kg): "))
-    acceleration = float(input("Enter object's acceleration (m/s^2): "))
-    print("The force applied on object is " + str(get_force(mass, acceleration)) + " N.")
-elif objective == "mass":
-    force = float(input("Enter force applied (N): "))
-    acceleration = float(input("Enter object's acceleration (m/s^2): "))
-    print("The mass of the object is " + get_mass(force, acceleration) + " kg.")
+if objective == "1": # Calculates force applied
+    mass = float(input("Mass (kg): "))
+    acceleration = float(input("Acceleration (m/s^2): "))
+    direction = input("Direction of Acceleration (Right/Left): ")
+    force = get_force(mass, acceleration)
+    if direction == "left":
+        force = -force
+    print("Force: " + str(force) + " N.")
+elif objective == "2": # Calculates object's mass
+    force = float(input("Force (N): "))
+    acceleration = float(input("Acceleration (m/s^2): "))
+    mass = get_mass(force, acceleration)
+    print("Mass: " + str(mass) + " kg.")
+elif objective == "3": # Calculates object's acceleration
+    force = float(input("Force: (N): "))
+    mass = float(input("Mass: (kg): "))
+    direction = input("Direction of Force (Right/Left): ")
+    acceleration = get_acceleration(force, mass)
+    if direction == "left":
+        acceleration = -acceleration
+    print("Acceleration: " + str(acceleration) + " m/s^2.")
 else:
-    force = float(input("Enter force applied (N): "))
-    mass = float(input("Enter object's mass (kg): "))
-    print("The acceleration of the object is " + get_acceleration(force, mass) + " m/s^2.")
+    print("Error: Unrecognized Request.")
